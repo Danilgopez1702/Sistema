730826f716adeeba41bd2b8e15202d237e976af9
Danilopez1702 <80540485+Danilgopez1702@users.noreply.github.com>
2025-08-04 13:17:00 -0600

    Update conexion.php

--- Aguascalientes/incluides/base_datos/conexion/conexion.php
+++ Aguascalientes/incluides/base_datos/conexion/conexion.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
-$host = "mocha3035.mochahost.com";
-$user = "inventar_admin";
-$clave = "Digitalayayayayayay1.";
-$bd = "inventar_sistemadn";
+$host = "localhost";
+$user = "root";
+$clave = "";
+$bd = "sistema";
 
 $conexion = mysqli_connect($host, $user, $clave, $bd);
 
