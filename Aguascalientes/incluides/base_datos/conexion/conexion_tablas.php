66030fe30ca1fc959c25c613be3cbd396c19cb49
Danilopez1702 <80540485+Danilgopez1702@users.noreply.github.com>
2025-08-04 13:18:05 -0600

    Update conexion_tablas.php

--- Aguascalientes/incluides/base_datos/conexion/conexion_tablas.php
+++ Aguascalientes/incluides/base_datos/conexion/conexion_tablas.php
@@ -4,10 +4,10 @@ class Conexion
 {
     public static function Conectar()
     {
-        define('servidor', 'mysql1005.mochahost.com');
-        define('nombre_bd', 'carlosfe_sistemadn');
-        define('usuario', 'carlosfe_sistemadn');
-        define('password', 'sistemadn');
+        define('servidor', 'localhost');
+        define('nombre_bd', 'sistema');
+        define('usuario', 'sistema');
+        define('password', 'sistema');
 
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
         try {
